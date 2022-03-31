5adf6798bbb8d1ceeead4c38114862569322ccc2
= <=>
2022-03-31 23:12:45 +0700

    project-iot

---
+++ TUT_LCD_I2C_F103_CUBEIDE/MDK-ARM/RTE/_TUT_LCD_I2C_F103_CUBEIDE/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'TUT_LCD_I2C_F103_CUBEIDE' 
+ * Target:  'TUT_LCD_I2C_F103_CUBEIDE' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ TUT_LCD_I2C_F103_CUBEIDE/MDK-ARM/TUT_LCD_I2C_F103_CUBEIDE/startup_stm32f103xb.d
@@ -0,0 +1,1 @@
+tut_lcd_i2c_f103_cubeide\startup_stm32f103xb.o: startup_stm32f103xb.s
